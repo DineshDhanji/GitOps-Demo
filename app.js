db3e5c4624cf1b81929b3e957276090b5bddb905
Dinesh Dhanjee <56204876+DineshDhanji@users.noreply.github.com>
2025-05-06 18:06:09 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("✅ Hello from GitOps with ArgoCD!\nV2");
+  res.send("✅ Hello from GitOps with ArgoCD! you there?");
 });
 
 app.listen(3000, () => {
