163d9cda2295816d86f459f0d18642d97dc8c1b7
Dinesh Dhanji <dhanjeehartik@gmail.com>
2025-05-08 20:34:42 +0500

    Refactor app.js to restore original greeting message and use dynamic port configuration

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("✅ Hello from GitOps with ArgoCD! you there? docker v1 updated");
-});
-
-app.listen(3000, () => {
-  console.log("App running on port 3000");
-});
+app.get("/", (req, res) => res.send("Hello from GitOps with Argo CD! (●'◡'●)"));
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
